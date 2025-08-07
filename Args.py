7110d8170ca5bf80ff13e2ba327a1fcc578a334b
kaleekun <kalaiselvanm@gmail.com>
2025-08-08 04:03:27 +0530

    Create Args.py

---
+++ Args.py
@@ -0,0 +1,16 @@
+# Author: KaLee
+
+def average(*args):
+  """Function to provide average of numbers provided.
+  Usage: average(1, 2, 3, 4, 5) returns 3 && average(1, 2, 3) returns 2. This support varying length of arguments"""
+  if not args:
+    return None # (or) raise ValueError('Values not provided')
+  return sum(args)/len(args)
+
+def centroid(*args):
+  """Function to provide centroid of any number of points in heterogenous dimensions
+  Usage: centroid([1, 2, 3], [1, 2], [1], [2, 3, 4, 5, 6, 7]) returns output as []"""
+  from itertools import zip_longest
+  if not args:
+    return None # (or) raise ValueError('Values not provided')
+  return [sum(i)/len(i) for i in zip_longest(*args, fillvalue = 0.0)]
