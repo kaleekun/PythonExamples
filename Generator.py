93ab94432165d313d630bc6a042c296b13ea396b
kaleekun <kalaiselvanm@gmail.com>
2025-10-15 14:52:45 +0530

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -4,16 +4,11 @@ import random
 from typing import Sequence
 def tria_gen(point1 :Sequence[float], point2 :Sequence[float], point3 :Sequence[float]):
     """Given three points in a list or tuple format, a generator is created yielding all points
-uniformly distributed.
-Usage: a = tria_gen([0, 0, 0], [1, 0,0], [1,1,0])
+       uniformly distributed.
+       Usage: a = tria_gen([0, 0, 0], [1, 0,0], [1,1,0])
        for i in range(10):
           print(next(a)) #-> this should print 10 random points inside the triangle provided
-       (or) you could write it to a file as shown below
-       with open('d:/temp/triaPoints.csv', 'w') as f:
-           for i in range(1000):
-               pt = next(a)
-               f.write(str(pt[0])+', ' + str(pt[1]) + ', ' + str(pt[2])+'\n')
-"""
+    """
     while True:
         a = random.random()        # assume this is a percentage
         b = (1-a)*random.random()  # reminder of the percentage's percentage
