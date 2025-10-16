a57ea3d3c31fd550d102a9ba13035abd846e9a4b
kaleekun <kalaiselvanm@gmail.com>
2025-10-17 02:03:07 +0530

    Create LineMagic.py

---
+++ LineMagic.py
@@ -0,0 +1,10 @@
+# Author: Kalaiselvan
+
+from IPython.core.magic import register_line_magic
+
+@register_line_magic # I could call magic functions as if they are executables that can be run in a command prompt
+def mymagic(args):
+  print(type(args))      # --> Will be always a string, that needs to be parsed and worked with
+  print(args.split())
+
+%mymagic kalai selvan   # Usage as here.
