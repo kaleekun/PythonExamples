475dbf266e7ea9aaaecdc92a15fccacc5344513c
kaleekun <kalaiselvanm@gmail.com>
2025-11-25 02:59:14 +0530

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -1,17 +1,35 @@
 # Author - KaLee
 
 from typing import Iterator
-def myRange(start: int, stop: int|None = None, step: int = 1)->Iterator[int|float]:
+import operator
+from itertools import zip_longest
+def myRange(start: int, stop: int|None = None, step: int|float = 1)->Iterator[int|float]:
   """This function is layed out as a generator to mimic 'range' builtin"""
   if stop is None:
     stop = start
     start = 0
-  i = start
-  if(stepSize > 0):
-    while(i < stop):
-      yield i
-      i += stepSize
-  elif(stepSize < 0):
-    while(i > stop):
-      yield i
-      i += stepSize
+  loopOp = operator.lt if step > 0 else operator.gt
+  if abs(step):
+    while(loopOp(start, stop)):
+      yield start
+      start += step
+
+
+for i in zip_longest(range(10), myRange(10)):
+  print(i)
+
+for i in zip_longest(range(0,10), myRange(0,10)):
+  print(i)
+
+for i in zip_longest(range(0,10, 2), myRange(0,10, 2)):
+  print(i)
+
+
+for i in zip_longest(range(0,10, -2), myRange(0,10, -2)):
+  print(i)
+
+for i in zip_longest(range(10,0, 2), myRange(10,0, 2)):
+  print(i)
+
+for i in zip_longest(range(10,0, -2), myRange(10,0, -2)):
+  print(i)
