4832236269872383775ccada605e21f1d510df0c
kaleekun <kalaiselvanm@gmail.com>
2025-11-25 02:13:29 +0530

    Create mydecorator.py

---
+++ mydecorator.py
@@ -0,0 +1,29 @@
+import functools
+
+def timedecorator(func): # takes in a function pointer
+    @functools.wraps     # --> makes sure, original functions documentations and required attributes are copied to innerfunc below
+    def innerfunc(*args, **kwds):
+        import time
+        start = time.time()
+        i = func(*args, **kwds)   # this is remembered
+        end = time.time()
+        print(b-a, " seconds")
+        return i # this matches with the return of actual function sent in.
+    return innerfunc # this return statement is for timedecorator function.
+# so far, with above code, what we have accomplished is that, wrapped the function that was passed.
+
+def logdecorator(func): # takes in a function pointer
+    @functools.wraps     # --> makes sure, original functions documentations and required attributes are copied to innerfunc below
+    def innerfunc(*args, **kwds):
+        with open(r"d:/temp/log.txt", "a") as file: # opening file
+          file.write("Function started to run\n")
+          i = func(*args, **kwds)   # this is remembered
+          file.write("Function finished running\n")
+          file.close() # close
+        return i # this matches with the return of actual function sent in.
+    return innerfunc # this return statement is for logdecorator function.
+
+@logdecorator   # this is a cascaded decorator
+@timedecorator  # this is equivalent to decoratedFunc = logdecorator(timedecorator(decoratedFunc))
+def decoratedFunc():
+    print('decoratedFunc is called')
