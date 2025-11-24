3eb01f0d50dc973fc898113a32c415a71771ed40
kaleekun <kalaiselvanm@gmail.com>
2025-11-24 22:37:47 +0530

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -19,3 +19,16 @@ def tria_gen(point1 :Sequence[float], point2 :Sequence[float], point3 :Sequence[
         yield [a*x1+b*x2+c*x3,
                 a*y1+b*y2+c*y3,
                 a*z1+b*z2+c*z3]
+
+def quad_gen(point1 :Sequence[float], point2 :Sequence[float], point3 :Sequence[float], point4: Sequence:[float]):
+    """Given four points in a list or tuple format, a generator is created yielding all points
+       uniformly distributed.
+       Usage: a = quad_gen([0, 0, 0], [0, -1,0], [1,1,0], [-1, 0, 0])
+       for i in range(10):
+          print(next(a)) #-> this should print 10 random points inside the quadilateral provided
+    """
+    a = tria_gen(point1, point2, point3) # using code of triagle generator to produce points for the quadilateral.
+    b = tria_gen(point3, point4, point1)
+    while True:
+        yield next(a)
+        yield next(b)
